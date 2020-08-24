8c682840ac063640c4bdcb4ac7b4ce364aaa5cb3
Owen Lockett <owenl@shinyshoe.com>
2020-08-23 20:19:04 -0700

    Add HellHornedFriends mod

---
+++ Mods/HellHornedFriends/FriendNameChange.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace HellHornedFriends
+{
+    [BepInPlugin("com.shinyshoe.friendnamechange", "Friend Name Change", "1.0.0.0")]
+    public class FriendNameChange : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            var harmony = new Harmony("com.shinyshoe.friendnamechange");
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ Mods/HellHornedFriends/NameChanger.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HellHornedFriends
+{
+    [HarmonyPatch(typeof(CharacterData))]
+    [HarmonyPatch("GetName")]
+    public static class ChangeFriendCharacterName
+    {
+        static void Postfix(ref string __result)
+        {
+            if (__result == "Alpha Fiend") __result = "Alpha Friend";
+            else if (__result == "Demon Fiend") __result = "Demon Friend";
+        }
+    }
+
+    [HarmonyPatch(typeof(CardState))]
+    [HarmonyPatch("GetTitle")]
+    public static class ChangeFriendCardName
+    {
+        static void Postfix(ref string __result)
+        {
+            if (__result == "Alpha Fiend") __result = "Alpha Friend";
+            else if (__result == "Demon Fiend") __result = "Demon Friend";
+        }
+    }
+
+}
