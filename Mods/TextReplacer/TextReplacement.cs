375d0e81e19ad2875883e5c60b8cdb5292dea8c2
Brendan Rafeld <bjrafeld@gmail.com>
2020-09-09 12:04:53 -0700

    Update TextReplacer Mod with data file to replace whatever text you want to.

---
+++ Mods/TextReplacer/TextReplacement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class TextReplacement
+{
+    public string sourceString;
+    public string targetString;
+
+    public static TextReplacement FromCSV(string line)
+    {
+        string[] values = line.Split(',');
+        TextReplacement replacement = new TextReplacement();
+        replacement.sourceString = values[0].Trim();
+        replacement.targetString = values[1].Trim();
+
+        return replacement;
+    }
+}
--- Mods/TextReplacer/TextReplacer.cs
+++ Mods/TextReplacer/TextReplacer.cs
@@ -1,38 +1,40 @@
 ﻿using BepInEx;
 using HarmonyLib;
-using ShinyShoe;
-using UnityEngine;
+using I2.Loc;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace OnlyStewards
 {
     [BepInPlugin("com.shinyshoe.textreplacer", "TextReplacer", "1.0.0.0")]
     public class TextReplacer : BaseUnityPlugin
     {
+        public static List<TextReplacement> Replacements;
+
         void Awake()
         {
+            var directory = Path.GetDirectoryName(Info.Location);
+            var path = Path.Combine(directory, "Replacements.csv");
+            Replacements = File.ReadAllLines(path)
+                .Select(r => TextReplacement.FromCSV(r))
+                .ToList();
+
             var harmony = new Harmony("com.shinyshoe.textreplacer");
             harmony.PatchAll();
         }
     }
 
-    [HarmonyPatch(typeof(CharacterData))]
-    [HarmonyPatch("GetName")]
-    public static class ChangeFriendCharacterName
+    [HarmonyPatch(typeof(LocalizationManager))]
+    [HarmonyPatch("GetTranslation")]
+    public static class Mod_LocationlizationManager_GetTranslation
     {
         static void Postfix(ref string __result)
         {
-            if (__result == "Little Fade") __result = "Shiny Steward";
-        }
-    }
-
-    [HarmonyPatch(typeof(CardState))]
-    [HarmonyPatch("GetTitle")]
-    public static class ChangeFriendCardName
-    {
-        static void Postfix(ref string __result)
-        {
-            if (__result == "Little Fade") __result = "Shiny Steward";
+            foreach(TextReplacement replacement in TextReplacer.Replacements)
+            {
+                __result = __result.Replace(replacement.sourceString, replacement.targetString);
+            }
         }
     }
 }
