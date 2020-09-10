92a6ba8e37ac565159abb0adcf6ae98f2b4a6277
Brendan Rafeld <bjrafeld@gmail.com>
2020-09-10 12:22:39 -0700

    Add MTSoap

---
+++ Mods/TrainsOfOurLives/FriendNameChange.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using UnityEngine;
+
+namespace HellHornedFriends
+{
+    [BepInPlugin("com.shinyshoe.mtsoap", "Trains of Our Lives", "1.0.0.0")]
+    public class FriendNameChange : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            var harmony = new Harmony("com.shinyshoe.mtsoap");
+            harmony.PatchAll();
+        }
+    }
+}
